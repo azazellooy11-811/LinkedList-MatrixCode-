fca1f98b0ff043e1d5db89bdc0533b66f75f645d
Азалия Хисматова <hismatova@outlook.com>
2019-03-06 00:14:40 +0300

    Добавьте файлы проекта.

---
+++ LinkedList-MatrixCode-/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections;
+
+public class LinkedList
+{
+    public class Node
+    {
+        public int data;
+
+        public Node right;
+
+        public Node down;
+
+        public int col;
+
+        public int row;
+    }
+
+    private static Node head;
+
+    private static int m = 3;
+
+    private static int n = 3;
+
+    private static Node construct(int[][] arr, int i, int j, int m, int n)
+    {
+        if (i > n - 1 || j > m - 1)
+        {
+            return null;
+        }
+        Node temp = new Node();
+        temp.data = arr[i][j];
+        temp.col = i;
+        temp.row = j;
+        temp.right = construct(arr, i, j + 1, m, n);
+        temp.down = construct(arr, i + 1, j, m, n);
+        return temp;
+    }
+
+    private static void MatrixCode(int[][] matrix)
+    {
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            matrix[i] = new int[matrix.Length];
+        }
+        head = construct(matrix, 0, 0, matrix.Length, matrix.Length);
+    }
+
+    private static int[][] decode()
+    {
+        int[][] matrix = new int[m][];
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            matrix[i] = new int[matrix.Length];
+        }
+        return matrix;
+    }
+
+    private static void insert(int i, int j, int value)
+    {
+        for (Node Dp = head; Dp != null; Dp = Dp.down)
+        {
+            for (Node Rp = Dp; Rp != null; Rp = Rp.right)
+            {
+                if (Rp.col == i && Rp.row == j)
+                {
+                    Rp.data = value;
+                }
+            }
+        }
+    }
+
+    private static void delete(int i, int j)
+    {
+        insert(i, j, 0);
+    }
+
+    private static ArrayList minList()
+    {
+        ArrayList minArr = new ArrayList();
+        for (int j = 0; j < m; j++)
+        {
+            int minValue = int.MaxValue;
+            for (int i = 0; i < m; i++)
+            {
+                int value = Read(i, j);
+                minValue = Math.Min(minValue, value);
+                Console.WriteLine("значение для строки:" + j + "x" + i + "=" + value + " min=" + minValue);
+            }
+            minArr.Add(minValue);
+            Console.WriteLine("Минимальное значение для столбца:" + j + "=" + minValue);
+        }
+        return minArr;
+    }
+
+    private static int sumDiag()
+    {
+        int sum = 0;
+        for (int i = 0; i < m; i++)
+        {
+            sum += Read(i, i);
+        }
+        return sum;
+    }
+
+    private static void transp()
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int i = j + 1; i < m; i++)
+            {
+                int val3 = Read(j, i);
+                int val2 = Read(i, j);
+                insert(i, j, val3);
+                insert(j, i, val2);
+                Console.WriteLine("-----Транспонирование-----");
+                display(head);
+            }
+        }
+    }
+
+    private static void sumCols(ref int j1, int j2)
+    {
+        int sum = 0;
+        for (int k = 0; k < m; k++)
+        {
+            sum += Read(k, j1);
+        }
+        for (int i = 0; i < m; i++)
+        {
+            sum += Read(i, j2);
+        }
+        j1 = sum;
+    }
+
+    private static int Read(int i, int j)
+    {
+        for (Node Dp = head; Dp != null; Dp = Dp.down)
+        {
+            for (Node Rp = Dp; Rp != null; Rp = Rp.right)
+            {
+                if (Rp.col == i && Rp.row == j)
+                {
+                    return Rp.data;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static void display(Node head)
+    {
+        for (Node Dp = head; Dp != null; Dp = Dp.down)
+        {
+            for (Node Rp = Dp; Rp != null; Rp = Rp.right)
+            {
+                Console.Write(Rp.data + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("-----------------");
+    }
+
+    public static void Main()
+    {
+        int[][] arr = new int[m][];
+        MatrixCode(arr);
+        insert(0, 0, 2);
+        insert(0, 1, 3);
+        insert(0, 2, 5);
+        insert(1, 0, 1);
+        insert(1, 1, 2);
+        insert(1, 2, 4);
+        insert(2, 0, 1);
+        insert(2, 1, 4);
+        insert(2, 2, 3);
+        display(head);
+        delete(0, 1);
+        display(head);
+        minList();
+        display(head);
+        Console.WriteLine("Сумма по главной диагонали:" + sumDiag());
+        int sum = 0;
+        sumCols(ref sum, 2);
+        Console.WriteLine("Сумма столбцов:" + sum);
+        transp();
+        display(head);
+        decode();
+        Console.ReadKey();
+    }
+}
---
+++ LinkedList-MatrixCode-/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("LinkedList-MatrixCode-")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LinkedList-MatrixCode-")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("14de0ed3-8e78-4c03-99e7-cd4700f4338a")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
